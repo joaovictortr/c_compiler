910013852e862c709b3b11808591d5d7ef1de658
Joao Victor Tozatti Risso <jvtr12@inf.ufpr.br>
2016-03-29 14:57:16 -0300

    Add basic tests
    Signed-off-by: Joao Victor Tozatti Risso <jvtr12@inf.ufpr.br>

---
+++ testes/teste.c
@@ -0,0 +1,13 @@
+// oi, eu sou um comentario
+main() {
+    /*
+     * Comentario de teste
+     */
+    // oi, eu sou um comentario
+    return 0;
+    // oi, eu sou um comentario
+} // oi, eu sou um comentario
+
+    /*
+     * Comentario de teste
+     */
