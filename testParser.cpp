420a1c320c4ec854269b8a4516ec174b56c8ded2
João Victor Risso <joaovictor.risso@gmail.com>
2016-06-19 00:11:44 -0300

    Add parsing of an example string
    Signed-off-by: João Victor Risso <joaovictor.risso@gmail.com>

--- testParser.cpp
+++ testParser.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <utility>
 #include <map>
+#include "Token.h"
+//#include "SymbolTable.h"
 #include "Parser.h"
 
 using namespace std;
@@ -11,6 +13,44 @@ using namespace std;
 int main(int argc, char **argv)
 {
     Parser parser;
+    //SymbolTable symTable;
+
+    string input("((a)a(aa))$");
+    vector< Token > inTokens;
+
+    for(char& c : input) {
+        inTokens.push_back( Token(c) );
+    }
+
+    for(auto it = inTokens.begin(); it != inTokens.end(); ++it) {
+        Token tok = *it;
+        if (!parser.parse(tok)) {
+            cerr << "Syntax error!" << endl;
+            return 1;
+        }
+    }
+
+    cout << "Input string \"" << input << "\" accepted!" << endl;
+
+    /*
+    Token t0 = Token('a');
+    cout << "t0.id = " << t0.id() << endl;
+
+    Token t1 = Token(TokenType::STRING);
+    cout << "t1.id = " << t1.id() << endl;
+
+    Token t2 = Token(TokenType::RELOP);
+    cout << "t2.id = " << t2.id() << endl;
+    */
+
+    /**
+    symTable.insert(t1.id(), "Hello world");
+    symTable.insert(t2.id(), "<=");
+
+    const size_t t2_id = t2.id();
+    SymbolTableElement t2_element = symTable[t2_id];
+    boost::apply_visitor(SymbolTableVisitor(), t2_element);
+    **/
 
     return 0;
 }
