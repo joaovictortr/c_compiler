fd4d4b23fd4f8f8465310667b9894154775d5aac
João Victor Risso <jvtr12@c3sl.ufpr.br>
2016-06-21 16:58:30 -0300

    Add statements to supress empty function warnings

--- src/SymbolTableVisitor.h
+++ src/SymbolTableVisitor.h
@@ -6,8 +6,8 @@ class SymbolTableVisitor
     : public boost::static_visitor<>
 {
 public:
-    void operator()(int & intVal) const {}
-    void operator()(float & floatVal) const {}
-    void operator()(char & charVal) const {}
-    void operator()(string & strVal) const {}
+    void operator()(int & intVal) const { intVal = intVal; }
+    void operator()(float & floatVal) const { floatVal = floatVal; }
+    void operator()(char & charVal) const { charVal = charVal; }
+    void operator()(string & strVal) const { strVal = strVal; }
 };
